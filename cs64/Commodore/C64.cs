27db1fadc1c37bc46f496c9719713cad1010e76a
Erik Sandberg <sandberg.erik@gmail.com>
2022-01-16 22:29:23 +0100

    Cleanup code

--- cs64/Commodore/C64.cs
+++ cs64/Commodore/C64.cs
@@ -1,29 +1,29 @@
 ﻿using MOS;
 
-namespace cs64
+namespace cs64;
+
+class C64
 {
-    class C64
-    {
-        public readonly RAMDevice Memory = new(new byte[1024 * 64]);
-        public uint[] VideoBuffer;
+    public readonly RAMDevice Memory = new(new byte[1024 * 64]);
+    public uint[] VideoBuffer;
 
-        private byte PORT_DIRECTIONAL_DATA_REGISTER { get => Memory.Data[0]; set => Memory.Data[0] = value; }
-        private byte PORT_REGISTER { get => Memory.Data[1]; set => Memory.Data[1] = value; }
-        private bool LORAM => (PORT_REGISTER & 1) == 1;
-        private bool HIRAM => (PORT_REGISTER & 2) == 2;
-        private bool CHAREN => (PORT_REGISTER & 4) == 4;
+    private byte PORT_DIRECTIONAL_DATA_REGISTER { get => Memory.Data[0]; set => Memory.Data[0] = value; }
+    private byte PORT_REGISTER { get => Memory.Data[1]; set => Memory.Data[1] = value; }
+    private bool LORAM => (PORT_REGISTER & 1) == 1;
+    private bool HIRAM => (PORT_REGISTER & 2) == 2;
+    private bool CHAREN => (PORT_REGISTER & 4) == 4;
 
 
-        // Devices
-        private readonly ROMDevice _kernal;
-        private readonly ROMDevice _chargen;
-        private readonly ROMDevice _basic;
+    // Devices
+    private readonly ROMDevice _kernal;
+    private readonly ROMDevice _chargen;
+    private readonly ROMDevice _basic;
 
-        private readonly RAMDevice _vicMemory = new(new byte[0x400]);
-        private readonly Fake6502 _cpu;
-        public readonly CIA _cia1;
+    private readonly RAMDevice _vicMemory = new(new byte[0x400]);
+    private readonly Fake6502 _cpu;
+    public readonly CIA _cia1;
 
-        private uint[] _palette = new uint[]{
+    private uint[] _palette = new uint[]{
             0x000000FFu,
             0xFFFFFFFFu,
             0x880000FFu,
@@ -41,161 +41,160 @@ namespace cs64
             0x0088FFFFu,
             0xBBBBBBFFu
         };
-        private readonly Random _random = new Random();
+    private readonly Random _random = new Random();
 
-        public C64()
+    public C64()
+    {
+        _kernal = new ROMDevice(File.ReadAllBytes("Roms/kernal"));
+        _basic = new ROMDevice(File.ReadAllBytes("Roms/basic"));
+        _chargen = new ROMDevice(File.ReadAllBytes("Roms/chargen"));
+
+        VideoBuffer = new uint[340 * 220];
+
+        _random.NextBytes(Memory.Data);
+
+        PORT_DIRECTIONAL_DATA_REGISTER = 0x2F;
+        PORT_REGISTER = 0x37;
+
+        _cpu = new Fake6502(true, Read6502, Write6502);
+        _cpu.Reset();
+
+        _cia1 = new CIA(_cpu.IssueIRQ, ReadCIA1, WriteCIA1);
+    }
+
+    byte ReadCIA1(CIA.Port port)
+    {
+        return 0;
+    }
+
+    void WriteCIA1(CIA.Port port, byte value)
+    {
+
+    }
+
+    private struct AddressDecodeResult
+    {
+        public bool CanWrite;
+        public IDevice Device;
+        public ushort Offset;
+
+        public AddressDecodeResult(bool canWrite, IDevice device, int offset)
         {
-            _kernal = new ROMDevice(File.ReadAllBytes("Roms/kernal"));
-            _basic = new ROMDevice(File.ReadAllBytes("Roms/basic"));
-            _chargen = new ROMDevice(File.ReadAllBytes("Roms/chargen"));
-
-            VideoBuffer = new uint[340 * 220];
-
-            _random.NextBytes(Memory.Data);
-
-            PORT_DIRECTIONAL_DATA_REGISTER = 0x2F;
-            PORT_REGISTER = 0x37;
-
-            _cpu = new Fake6502(true, Read6502, Write6502);
-            _cpu.Reset();
-
-            _cia1 = new CIA(_cpu.IssueIRQ, ReadCIA1, WriteCIA1);
+            CanWrite = canWrite;
+            Device = device;
+            Offset = (ushort)offset;
         }
+    }
 
-        byte ReadCIA1(CIA.Port port)
+    private AddressDecodeResult DecodeAddress(ushort address)
+    {
+        var charRomEnabled = !CHAREN && (HIRAM || LORAM);
+        var ioEnabled = CHAREN && (HIRAM || LORAM);
+        var basicEnabled = HIRAM && LORAM;
+        var kernalEnabled = HIRAM;
+
+        if (kernalEnabled && address >= 0xE000 && address <= 0xFFFF)
         {
-            return 0;
+            return new AddressDecodeResult(false, _kernal, address - 0xE000);
         }
-
-        void WriteCIA1(CIA.Port port, byte value)
+        else if (basicEnabled && address >= 0xA000 && address <= 0xBFFF)
         {
-
+            return new AddressDecodeResult(false, _basic, address - 0xA000);
         }
-
-        private struct AddressDecodeResult
+        else if (address >= 0xD000 && address <= 0xDFFF)
         {
-            public bool CanWrite;
-            public IDevice Device;
-            public ushort Offset;
-
-            public AddressDecodeResult(bool canWrite, IDevice device, int offset)
+            if (charRomEnabled)
             {
-                CanWrite = canWrite;
-                Device = device;
-                Offset = (ushort)offset;
+                return new AddressDecodeResult(false, _chargen, address - 0xD000);
             }
-        }
-
-        private AddressDecodeResult DecodeAddress(ushort address)
-        {
-            var charRomEnabled = !CHAREN && (HIRAM || LORAM);
-            var ioEnabled = CHAREN && (HIRAM || LORAM);
-            var basicEnabled = HIRAM && LORAM;
-            var kernalEnabled = HIRAM;
-
-            if (kernalEnabled && address >= 0xE000 && address <= 0xFFFF)
+            else if (ioEnabled)
             {
-                return new AddressDecodeResult(false, _kernal, address - 0xE000);
-            }
-            else if (basicEnabled && address >= 0xA000 && address <= 0xBFFF)
-            {
-                return new AddressDecodeResult(false, _basic, address - 0xA000);
-            }
-            else if (address >= 0xD000 && address <= 0xDFFF)
-            {
-                if (charRomEnabled)
+                if (address >= 0xD000 && address <= 0xD3FF)
                 {
-                    return new AddressDecodeResult(false, _chargen, address - 0xD000);
+                    return new AddressDecodeResult(true, _vicMemory, address & 0x3F);
                 }
-                else if (ioEnabled)
+                else if (address >= 0xDC00 && address <= 0xDCFF)
                 {
-                    if(address >= 0xD000 && address <= 0xD3FF)
-                    {
-                        return new AddressDecodeResult(true, _vicMemory, address & 0x3F);
-                    }
-                    else if(address >= 0xDC00 && address <= 0xDCFF)
-                    {
-                        return new AddressDecodeResult(true, _cia1, address & 0x0F);
-                    }
-                    //else if(address >= 0xDD00 && address <= 0xDDFF)
-                    //{
-                    //    // CIA2
-                    //}
+                    return new AddressDecodeResult(true, _cia1, address & 0x0F);
                 }
-            }
-
-            return new AddressDecodeResult(true, Memory, address);
-        }
-
-        private byte Read6502(ushort address)
-        {
-            var result = DecodeAddress(address);
-            return result.Device.Read(result.Offset);
-        }
-
-        private void Write6502(ushort address, byte value)
-        {
-            var result = DecodeAddress(address);
-            if (result.CanWrite)
-                result.Device.Write(result.Offset, value);
-        }
-
-        const ushort Vic2_BaseAddress = 0xD000;
-        const ushort Vic2_CurrentRasterLine = 0xD012 - Vic2_BaseAddress;
-        const ushort Vic2_BorderColor = 0xD020 - Vic2_BaseAddress;
-        const ushort Vic2_BackgroundColor = 0xD021 - Vic2_BaseAddress;
-        const ushort Vic2_ScreenControlRegister1 = 0xD011 - Vic2_BaseAddress;
-        const ushort Vic2_ScreenControlRegister2 = 0xD016 - Vic2_BaseAddress;
-
-        const ushort ColorRam_BaseAddress = 0xD800;
-
-        private void ExecTicks(uint tickcount)
-        {
-            var clockgoal6502 = _cpu.ClockTicks6502 + tickcount;
-
-            while (_cpu.ClockTicks6502 < clockgoal6502)
-            {
-                _cpu.Step();
-                _cia1.Step();
+                //else if(address >= 0xDD00 && address <= 0xDDFF)
+                //{
+                //    // CIA2
+                //}
             }
         }
 
-        public void RunFrame()
+        return new AddressDecodeResult(true, Memory, address);
+    }
+
+    private byte Read6502(ushort address)
+    {
+        var result = DecodeAddress(address);
+        return result.Device.Read(result.Offset);
+    }
+
+    private void Write6502(ushort address, byte value)
+    {
+        var result = DecodeAddress(address);
+        if (result.CanWrite)
+            result.Device.Write(result.Offset, value);
+    }
+
+    const ushort Vic2_BaseAddress = 0xD000;
+    const ushort Vic2_CurrentRasterLine = 0xD012 - Vic2_BaseAddress;
+    const ushort Vic2_BorderColor = 0xD020 - Vic2_BaseAddress;
+    const ushort Vic2_BackgroundColor = 0xD021 - Vic2_BaseAddress;
+    const ushort Vic2_ScreenControlRegister1 = 0xD011 - Vic2_BaseAddress;
+    const ushort Vic2_ScreenControlRegister2 = 0xD016 - Vic2_BaseAddress;
+
+    const ushort ColorRam_BaseAddress = 0xD800;
+
+    private void ExecTicks(uint tickcount)
+    {
+        var clockgoal6502 = _cpu.ClockTicks6502 + tickcount;
+
+        while (_cpu.ClockTicks6502 < clockgoal6502)
         {
-            for (byte line = 0; line < 220; line++)
+            _cpu.Step();
+            _cia1.Step();
+        }
+    }
+
+    public void RunFrame()
+    {
+        for (byte line = 0; line < 220; line++)
+        {
+            _vicMemory.Data[Vic2_CurrentRasterLine] = line;
+
+            ExecTicks(64);
+
+            byte borderColor = _vicMemory.Data[Vic2_BorderColor];
+            byte bgColor = _vicMemory.Data[Vic2_BackgroundColor];
+
+            byte h_scroll = _vicMemory.Data[Vic2_ScreenControlRegister2 & 7];
+            byte v_scroll = _vicMemory.Data[Vic2_ScreenControlRegister1 & 7];
+
+            byte char_y = (byte)((line - 10 - v_scroll) / 8);
+            byte char_offset_y = (byte)((line - 10 - v_scroll) % 8);
+
+            for (var x = 0; x < 340; x++)
             {
-                _vicMemory.Data[Vic2_CurrentRasterLine] = line;
-
-                ExecTicks(64);
-
-                byte borderColor = _vicMemory.Data[Vic2_BorderColor];
-                byte bgColor = _vicMemory.Data[Vic2_BackgroundColor];
-
-                byte h_scroll = _vicMemory.Data[Vic2_ScreenControlRegister2 & 7];
-                byte v_scroll = _vicMemory.Data[Vic2_ScreenControlRegister1 & 7];
-
-                byte char_y = (byte)((line - 10 - v_scroll) / 8);
-                byte char_offset_y = (byte)((line - 10 - v_scroll) % 8);
-
-                for (var x = 0; x < 340; x++)
+                byte color;
+                if (x < 10 || x > 329 || (line - v_scroll) < 10 || ((line - v_scroll) % 220) > 209)
+                    color = borderColor;
+                else
                 {
-                    byte color;
-                    if (x < 10 || x > 329 || (line - v_scroll) < 10 || ((line - v_scroll) % 220) > 209)
-                        color = borderColor;
-                    else
-                    {
-                        byte char_x = (byte)((x - 10) >> 3);
-                        byte char_offset_x = (byte)((x - 10) % 8);
+                    byte char_x = (byte)((x - 10) >> 3);
+                    byte char_offset_x = (byte)((x - 10) % 8);
 
-                        byte chr = Memory.Data[0x400 + (char_y * 40) + char_x];
-                        byte chr_fnt = _chargen.Data[(chr * 8) + char_offset_y];
+                    byte chr = Memory.Data[0x400 + (char_y * 40) + char_x];
+                    byte chr_fnt = _chargen.Data[(chr * 8) + char_offset_y];
 
-                        bool on = ((chr_fnt << char_offset_x) & 0x80) != 0;
-                        color = on ? Memory.Data[ColorRam_BaseAddress + (char_y * 40) + char_x] : bgColor;
-                    }
-
-                    VideoBuffer[line * 340 + ((x + h_scroll) % 340)] = _palette[color % 16];
+                    bool on = ((chr_fnt << char_offset_x) & 0x80) != 0;
+                    color = on ? Memory.Data[ColorRam_BaseAddress + (char_y * 40) + char_x] : bgColor;
                 }
+
+                VideoBuffer[line * 340 + ((x + h_scroll) % 340)] = _palette[color % 16];
             }
         }
     }
